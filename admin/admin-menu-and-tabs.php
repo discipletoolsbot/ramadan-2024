1284cf8ec6303782193026504dce3dd16257827e
corsacca <corsacca@gmail.com>
2024-05-14 12:24:28 +0100

    Changes for admin UI

--- admin/admin-menu-and-tabs.php
+++ admin/admin-menu-and-tabs.php
@@ -47,32 +47,7 @@ Ramadan_2024_Menu::instance();
  */
 class Ramadan_2024_Tab_General {
     public static function content() {
-
-
-        ?>
-        <div class="wrap">
-            <div id="poststuff">
-                <div id="post-body" class="metabox-holder columns-2">
-                    <div id="post-body-content">
-                        <!-- Main Column -->
-
-                        <?php self::main_column() ?>
-
-                        <!-- End Main Column -->
-                    </div><!-- end post-body-content -->
-                    <div id="postbox-container-1" class="postbox-container">
-                        <!-- Right Column -->
-
-                        <?php self::right_column() ?>
-
-                        <!-- End Right Column -->
-                    </div><!-- postbox-container 1 -->
-                    <div id="postbox-container-2" class="postbox-container">
-                    </div><!-- postbox-container 2 -->
-                </div><!-- post-body meta box container -->
-            </div><!--poststuff end -->
-        </div><!-- wrap end -->
-        <?php
+        self::main_column();
     }
 
     public static function main_column() {
